efd210c86e52dc8d58d64eb9bca2d672c9728da8
Alex Sobral <alex.dacostasobral@gmail.com>
2023-10-18 10:16:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoDB = require('mongodb');
+const mongoose = require('mongoose');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+const uri = "mongodb+srv://alex:04177110@alexsobraldb.fargf4z.mongodb.net/scheduling-system?retryWrites=true&w=majority";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log('connected to mongoDB!')
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connect();
+
+
+app.get('/', (req, res) => {
+  res.send('working')
+})
+
+app.listen(3001, () => console.log('server working!'))
